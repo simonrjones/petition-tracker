541e26490f9c4d3ecb20b8db1fc9ab6d7c0b8eef
Simon R Jones <simon@studio24.net>
2017-01-31 21:50:42 +0000

    Typo

--- petitions.php
+++ petitions.php
@@ -2,7 +2,7 @@
 
 // Array of petition IDs => HTML page to create
 $petitions = [
-    131215 => 'brexit',
+    131215 => 'brexit.html',
     171928 => 'trump-no-state-visit.html',
     178844 => 'trump-yes-state-visit.html'
 ];
