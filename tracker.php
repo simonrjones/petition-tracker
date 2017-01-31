4405233f4bbad5eaa8780c2b317c587a12dd3ce3
Simon R Jones <simon@studio24.net>
2017-01-31 21:01:40 +0000

    Enable support for multiple petitions

--- tracker.php
+++ tracker.php
@@ -8,49 +8,61 @@ date_default_timezone_set('Europe/London');
 
 require 'vendor/autoload.php';
 
+// Array of petition IDs => HTML page to create
+$petitions = [
+    171928 => 'index.html',
+    178844 => 'trump.html'
+];
+
 $adapter = new Local(__DIR__);
 $filesystem = new Filesystem($adapter);
 
-if ($filesystem->has('signature_count.txt')) {
-    $signature_count = json_decode($filesystem->read('signature_count.txt'));
-    if ($signature_count === false) {
-        throw new Exception('Cannot decode data at signature_count.txt');
+function trackPetition($petitionId, $htmlOutput)
+{
+    $version = '1.0.1';
+
+    $signatureCountFile = 'signature_count_' . $petitionId . '.txt';
+    if ($filesystem->has($signatureCountFile)) {
+        $signature_count = json_decode($filesystem->read($signatureCountFile));
+        if ($signature_count === false) {
+            throw new Exception('Cannot decode data at ' . $signatureCountFile);
+        }
+    } else {
+        $signature_count = [];
     }
-} else {
-    $signature_count = [];
-}
 
-try {
-    $client = new GuzzleHttp\Client();
-    $res = $client->request('GET', 'https://petition.parliament.uk/petitions/171928.json', [
-        'headers' => [
-            'User-Agent' => 'Petition-Tracker/1.0 (simon@studio24.net)'
-        ]
-    ]);
+    try {
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://petition.parliament.uk/petitions/' . $petitionId . '.json', [
+            'headers' => [
+                'User-Agent' => 'Petition-Tracker/' . $version . ' (simon@studio24.net)'
+            ]
+        ]);
 
-    if ($res->getStatusCode() == 200) {
+        if ($res->getStatusCode() == 200) {
 
-        $data = json_decode($res->getBody());
-        $now = new DateTime();
+            $data = json_decode($res->getBody());
+            $now = new DateTime();
 
-        $signature_count[] = [
-            'date'            => $now->format('r'),
-            'signature_count' => $data->data->attributes->signature_count
-        ];
+            $signature_count[] = [
+                'date'            => $now->format('r'),
+                'signature_count' => $data->data->attributes->signature_count
+            ];
 
-        $filesystem->put('signature_count.txt', json_encode($signature_count, JSON_PRETTY_PRINT));
+            $filesystem->put($signatureCountFile, json_encode($signature_count, JSON_PRETTY_PRINT));
 
-        echo "Count: " . $data->data->attributes->signature_count . PHP_EOL;
+            echo "Count for petition ID $petitionId: " . $data->data->attributes->signature_count . PHP_EOL;
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage() . PHP_EOL;
     }
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . PHP_EOL;
-}
 
-// Generate page
-ob_start();
-require 'stats.php';
-$contents = ob_get_clean();
-$filesystem->put('web/index.html', $contents);
+    // Generate stats page
+    ob_start();
+    require 'stats.php';
+    $contents = ob_get_clean();
+    $filesystem->put('web/' . $htmlOutput, $contents);
+}
 
 echo 'All done!' . PHP_EOL;
 
