a484d36a1ccd538e5d65b7d644a24de66d689899
Simon R Jones <simon@studio24.net>
2019-08-28 11:23:53 +0100

    add petition

--- petitions.php
+++ petitions.php
@@ -7,4 +7,5 @@ $petitions = [
     178844 => 'trump-yes-state-visit.html',
     180272 => 'science-funding.html',
     241584 => 'revoke-article-50.html',
+	269157 => 'do-not-prorogue-parliament.html'
 ];
